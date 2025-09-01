7a03baa6342cffb7c96e202d6add5b304d9a10ac
Shivam461221 <shivamlowanshi@gmail.com>
2025-09-01 13:37:56 +0530

    Javascript datatype, Variables

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,40 @@ console.log('Hello Js');
 
 console.log('External JS file');
 
+//variables, data types 
+
+//variables - these are containers to hold data during runtime of a program
+//let, const, var
+
+//data types - Primitive and Non-primitive data types 
+
+//primitive - number, string, boolean (true, false), null, undefined, bigint
+
+//non-primitive - array, object, function
+
+let firstName = 'Shivam'; //  "", ``
+
+console.log(typeof firstName);
+
+let age = 25.2312;
+
+console.log(typeof age);
+
+let isMarried = true;
+
+console.log(typeof isMarried);
+
+let salary = null;
+
+let x;
+
+console.log(typeof x);
+
+let phone = 1212121212n;
+
+console.log(typeof phone);
+
+
+
+
+
